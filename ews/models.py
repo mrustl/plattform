c58998595bbd89009c049f6f4b0af339ecf29a62
wseis <wolfgang.seis@posteo.de>
2020-08-04 17:15:01 +0200

    change base.html + loging logout

--- ews/models.py
+++ ews/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class BathingSpot(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="user")
     name=models.CharField(max_length= 64)
     def __str__(self):
         return f"{self.name}"
--- plattform/settings.py
+++ plattform/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'plattform.wsgi.application'
-
+LOGIN_REDIRECT_URL = '/'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
